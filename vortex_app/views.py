6007af775fadf2210ec456d5f31adf929c70e485
mihirsolanki3 <smihir118@gmail.com>
2024-03-15 20:06:45 +0530

    Unwanted code removed

--- vortex_app/views.py
+++ vortex_app/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
     
 class ProfileView(APIView):
