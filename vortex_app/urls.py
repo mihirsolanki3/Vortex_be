0ad9ca77684c9cb54c07b97efb6f58fbd913fe43
pratiksolanki2004 <pratik.solanki@digiqt.com>
2024-04-15 20:25:00 +0530

    Unsplash Integrated

--- vortex_app/urls.py
+++ vortex_app/urls.py
@@ -1,23 +1,24 @@
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib import admin
 from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'user',views.UserViewSet)
-router.register(r'content',views.ContentViewSet)
-router.register(r'content_moderation',views.Content_ModerationViewSet)
-router.register(r'payments',views.PaymentsViewSet)
-router.register(r'advertisements',views.AdvertisementsViewSet)
-router.register(r'subscriptions',views.SubscriptionsViewSet)
-router.register(r'content_interaction',views.ContentInteractionViewSet)
-router.register(r'search',views.Search_RetrievalViewSet)
-router.register(r'source',views.SourceViewSet)
-router.register(r'category',views.CategoryViewSet)
-router.register(r'fileupload',views.FileUploadViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'content', views.ContentViewSet)
+router.register(r'content_moderation', views.Content_ModerationViewSet)
+router.register(r'payments', views.PaymentsViewSet)
+router.register(r'advertisements', views.AdvertisementsViewSet)
+router.register(r'subscriptions', views.SubscriptionsViewSet)
+router.register(r'content_interaction', views.ContentInteractionViewSet)
+router.register(r'search', views.Search_RetrievalViewSet)
+router.register(r'source', views.SourceViewSet)
+router.register(r'category', views.CategoryViewSet)
+router.register(r'fileupload', views.FileUploadViewSet)
 
 
 urlpatterns = [
-    path('',include(router.urls)),
-    path('profile/', views.ProfileView.as_view())
-] 
+    path('', include(router.urls)),
+    path('profile/', views.ProfileView.as_view()),
+    path('photo/', views.download_photo_from_unsplash)
+]
--- vortex_app/views.py
+++ vortex_app/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,10 +10,21 @@ from vortex_app.models import *
 from vortex_app.serializer import *
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
-#from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.core.files.base import ContentFile
+from rest_framework.pagination import PageNumberPagination
 
-#User = get_user_model()
-    
+
+class FileUploadPagination(PageNumberPagination):
+    page_size = 10  # Adjust the page size as needed
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
+
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -23,50 +36,86 @@ class ProfileView(APIView):
             'last_name': str(request.user.last_name),
         }
         return Response(content)
-    
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = User_Serializer
-    
+
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = Category_Serializer
-    
+
+
 class FileUploadViewSet(viewsets.ModelViewSet):
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    queryset = FileUpload.objects.all()
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    queryset = FileUpload.objects.all().order_by('?')
     serializer_class = FileUpload_Serializer
     search_fields = ['category__category_name']
     filterset_fields = ['category']
-    
+    pagination_class = FileUploadPagination
+
+
 class ContentViewSet(viewsets.ModelViewSet):
-    queryset=Content.objects.all()
-    serializer_class=Content_Serializer
+    queryset = Content.objects.all()
+    serializer_class = Content_Serializer
+
 
 class Content_ModerationViewSet(viewsets.ModelViewSet):
-    queryset=Content_Moderation.objects.all()
-    serializer_class=Content_Moderation_Serializer   
+    queryset = Content_Moderation.objects.all()
+    serializer_class = Content_Moderation_Serializer
+
 
 class PaymentsViewSet(viewsets.ModelViewSet):
-    queryset=Payments.objects.all()
-    serializer_class=Payments_Serializer   
+    queryset = Payments.objects.all()
+    serializer_class = Payments_Serializer
+
 
 class AdvertisementsViewSet(viewsets.ModelViewSet):
-    queryset=Advertisements.objects.all()
-    serializer_class=Advertisements_Serializer    
+    queryset = Advertisements.objects.all()
+    serializer_class = Advertisements_Serializer
+
 
 class SubscriptionsViewSet(viewsets.ModelViewSet):
-    queryset=Subscriptions.objects.all()
-    serializer_class=Subscriptions_Serializer      
+    queryset = Subscriptions.objects.all()
+    serializer_class = Subscriptions_Serializer
+
 
 class ContentInteractionViewSet(viewsets.ModelViewSet):
-    queryset=ContentInteraction.objects.all()
-    serializer_class=ContentInteraction_Serializer 
+    queryset = ContentInteraction.objects.all()
+    serializer_class = ContentInteraction_Serializer
+
 
 class Search_RetrievalViewSet(viewsets.ModelViewSet):
-    queryset=Search_Retrieval.objects.all()
-    serializer_class=Search_Retrieval_Serializer        
+    queryset = Search_Retrieval.objects.all()
+    serializer_class = Search_Retrieval_Serializer
+
 
 class SourceViewSet(viewsets.ModelViewSet):
-    queryset=Source.objects.all()
-    serializer_class=Source_Serializer          
+    queryset = Source.objects.all()
+    serializer_class = Source_Serializer
+
+
+def download_photo_from_unsplash(request):
+    categories = Category.objects.all()
+    for category in categories:
+        for page in range(1, 100):
+            response = requests.get(
+                "https://api.unsplash.com/search/photos/", params={"client_id": "zF8Ku92rcNtoldkP2sKie1-Vs8h9B6OK9LIqTKoDrdM", "query": category.category_name, "page": page}
+            )
+            if response.status_code == 200:
+                photo_data = response.json()['results']
+                for picture in photo_data:
+                    photo_url = picture.get("urls").get("full")
+                    photo_response = requests.get(photo_url)
+                    if photo_response.status_code == 200:
+                        photo_content = photo_response.content
+
+                        photo = FileUpload()
+                        photo.category = category
+                        photo.image.save(
+                            f'{picture.get("slug")}.jpg', ContentFile(photo_content))
+                        photo.save()
+        return JsonResponse({"status": "success", "message": "Downloaded"})
